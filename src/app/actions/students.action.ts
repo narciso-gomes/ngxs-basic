e6ea6665d776eab91ad98facbb17f5dc6ef78f9d
Narciso Gomes <narcisoalmeida25@gmail.com>
2022-03-28 00:44:04 -0300

    student store

---
+++ src/app/actions/students.action.ts
@@ -0,0 +1,30 @@
+import { Student } from './../interfaces/student.interface';
+
+
+/**
+ * Action to add student
+ */
+export class AddStudent {
+  static readonly type = '[STUDENT] Add';
+
+  /**
+   * Constructor
+   * @param student - Student to add
+   */
+  constructor(public student: Student) {}
+}
+
+/**
+ * Action to remove a student
+ */
+export class RemoveStudent {
+  static readonly type = '[STUDENT] Add';
+
+  /**
+   * Constructor
+   * @param id - Student id
+   */
+  constructor(public id: number) {}
+}
+
+
---
+++ src/app/states/students.state.ts
@@ -0,0 +1,50 @@
+import { AddStudent, RemoveStudent } from './../actions/students.action';
+import { Action, Selector, State, StateContext } from '@ngxs/store';
+import { Student } from './../interfaces/student.interface';
+
+export class StudentStateModel {
+  students: Student[] = [];
+}
+
+@State<StudentStateModel>({
+  name: 'students',
+  defaults: {
+    students: [],
+  },
+})
+export class StudentState {
+  /**
+   * Selector all students
+   * @param state - students state
+   * @returns - all item from students state
+   */
+  @Selector()
+  static getStudents(state: StudentStateModel) {
+    return state.students;
+  }
+
+  /**
+   * Action to add student
+   * @param {getState, patchState} - students state
+   * @param {student} - Student from AddStudent action
+   */
+  @Action(AddStudent)
+  add({ getState, patchState }: StateContext<StudentStateModel>,{ student }: AddStudent) {
+    const state = getState();
+    patchState({
+      students: [...state.students, student],
+    });
+  }
+
+  /**
+   * Action to remove student
+   * @param {getState, patchState} - students state
+   * @param {id} - id from RemoveStudent action
+   */
+  @Action(RemoveStudent)
+  remove({ getState, patchState }: StateContext<StudentStateModel>,{ id }: RemoveStudent) {
+    patchState({
+      students: getState().students.filter((a) => a.regId != id),
+    });
+  }
+}
